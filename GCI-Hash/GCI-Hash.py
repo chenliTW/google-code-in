08bc2e68fb314fa1e12cea212b836d28cfeff204
chenlitw <ab0897867564534231@gmail.com>
2019-12-11 20:24:22 +0800

    GCI-Hash finished without online cracking function

--- GCI-Hash/GCI-Hash.py
+++ GCI-Hash/GCI-Hash.py
@@ -27,7 +27,16 @@ def do_hash(method,word):
         sha512.update(word.encode())
         return sha512.hexdigest()
     
-    
+def burte_hash(hash_target,wordlist):
+    wordlist=wordlist.split('\n')
+    for word in wordlist: 
+        for method in range(1,7):
+            if str(do_hash(str(method),word))==hash_target:
+                print("\033[92m"+"[+]found --> "+hash_target+" = "+word+"\033[0m")
+                return
+    print("\033[91m"+"[-]sorry no word match hash"+"\033[0m")
+    return
+
 if __name__=="__main__":
     while True:
         decision=str(input("To Hash press 1 and to Crack Hash press 2 :"))
@@ -37,6 +46,13 @@ if __name__=="__main__":
             plain_string=str(input("Enter the thing to be Hashed:"))
             print(plain_string+" = "+do_hash(hash_decision,plain_string))
         elif decision=="2":
-            pass
+            hashed_string=str(input("Enter the Hash :"))
+            word_list_path=str(input("Enter name of password list :"))
+            try:            
+                word_list=open("./"+word_list_path,"r")
+            except FileNotFoundError:
+                print("\033[91m"+"[-]wordlist file not found!!!"+"\033[0m")
+                continue
+            burte_hash(hashed_string,word_list.read())
         else:
             print("unsupported command")
