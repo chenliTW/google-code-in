c08b303df6e317a1ddf7da9f639782367f5c5e6c
chenlitw <ab0897867564534231@gmail.com>
2019-12-15 01:04:59 +0800

    update

---
+++ Spell_checker/spell_check.py
@@ -0,0 +1,9 @@
+from spellchecker import SpellChecker
+
+spell = SpellChecker()
+
+if __name__=="__main__":
+    word=input("Enter the paragraph :")
+    word=word.split()
+    for i in word:
+        print(spell.correction(i))
