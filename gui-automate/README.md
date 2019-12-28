91c6500e312b8d5583820fb45b479e7eba14ecc6
chenlitw <ab0897867564534231@gmail.com>
2019-12-28 21:12:35 +0800

    update

---
+++ gui-automate/README.md
@@ -0,0 +1,8 @@
+to make the script auto start.
+add
+```
+sudo python3 {path to this dir}/app.py
+```
+to the buttom of /etc/profile file
+
+And that all :)
--- gui-automate/app.py
+++ gui-automate/app.py
@@ -1,7 +1,29 @@
 import tkinter as tk
+import webbrowser
+import os
 
-gui=tk.Tk()
 
+def open_github():
+    webbrowser.open(url="https://github.com/")
 
+def open_myplaylist():
+    playlist="/home/chen/文件/GitHub/google-code-in/gui-automate/playlist/playlist.m3u"
+    os.system('xdg-open '+playlist)    
 
-gui.mainloop()
+def update_computer():
+    os.system("sudo apt-get update && sudo apt-get dist-upgrade -y")
+
+def main():
+    gui=tk.Tk()
+    gui.geometry('500x50')
+    gui.title('Python automate')
+    button_1=tk.Button(text='Open Github',command=open_github)
+    button_1.place(x=0,y=0)
+    button_2=tk.Button(text='Play playlist',command=open_myplaylist)
+    button_2.place(x=150,y=0)
+    button_3=tk.Button(text='update computer',command=update_computer)
+    button_3.place(x=300,y=0)
+    gui.mainloop()
+
+if __name__=="__main__":
+    main()
