ff66191c648c2d6173067373d86ec0f95082c646
chenlitw <ab0897867564534231@gmail.com>
2019-12-29 15:44:00 +0800

    update

--- google-news/app.py
+++ google-news/app.py
@@ -1,17 +1,15 @@
 from newsapi import NewsApiClient
 import pydoc
-import pprint
 
-api_key='9ebb83fde9d84aa2bf624dcc350179c5'
+api_key=''
 
 newsapi = NewsApiClient(api_key=api_key)
 
-query=['Linux','','']
+query=['Linux','Open-source','Android']
 for i in query:
     all_articles = newsapi.get_everything(q=i,sort_by='publishedAt',page=1)
     all_articles=all_articles['articles']
     output=str()
-    pprint.pprint(all_articles)
     for j in all_articles:
-        output+="title: {}\ndate: {}\ncontent:{}".format(j['title'],j['publishedAt'],j['description'])
+        output+="title: {}\n - author: {}\n - date: {}\n - description:{}\n - url: {}\n\n".format(j['title'],j['author'],j['publishedAt'],j['description'],j['url'])
     pydoc.pager(output)
