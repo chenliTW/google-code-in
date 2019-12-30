2e9d358b21d4ba4a62de2625026ef81383f02b15
chen <ab0897867564534231@gmail.com>
2019-12-30 19:14:56 +0800

    use flask

--- google-news/app.py
+++ google-news/app.py
@@ -1,15 +1,27 @@
 from newsapi import NewsApiClient
-import pydoc
+from flask import Flask
+
 
 api_key=''
 
 newsapi = NewsApiClient(api_key=api_key)
 
-query=['Linux','Open-source','Android']
-for i in query:
-    all_articles = newsapi.get_everything(q=i,sort_by='publishedAt',page=1)
-    all_articles=all_articles['articles']
-    output=str()
-    for j in all_articles:
-        output+="title: {}\n - author: {}\n - date: {}\n - description:{}\n - url: {}\n\n".format(j['title'],j['author'],j['publishedAt'],j['description'],j['url'])
-    pydoc.pager(output)
+app=Flask(__name__)
+
+@app.route('/')
+def index():
+    query=['Linux','Open-source','Android']
+    output="<html><body>"
+    for i in query:
+        output+="<details><summary>{}</summary>".format(i)
+        all_articles = newsapi.get_everything(q=i,sort_by='publishedAt',page=1)
+        all_articles=all_articles['articles']
+        for j in all_articles:
+            output+="<table border='1' width='100%'>"
+            output+="<tr><th>{}</th></tr><tr><td> - author: {}</tr></td><tr><td> - date: {}</tr></td><tr><td> - description:{}</tr></td><tr><td> - url: <a href='{}'>full article link</a></tr></td>".format(j['title'],j['author'],j['publishedAt'],j['description'],j['url'])
+            output+="</table>"
+        output+="</details>"
+    output+="</html></body>"
+    return output
+
+app.run()
